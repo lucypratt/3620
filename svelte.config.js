829613efc6857e89fc358a906ccece429c04998c
Lucy Steckler <lucy.steckler@gmail.com>
2024-04-27 21:24:01 -0600

    reverted changes

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,11 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-        appDir: 'app', // Required as the default is _app
-        adapter: adapter(),
-		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
